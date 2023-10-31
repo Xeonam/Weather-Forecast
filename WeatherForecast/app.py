9c5f1e2b18436fe039127739952c9ff281793aff
Balázs Sidlóczki <sidbalazs18@gmail.com>
2023-10-31 19:07:49 +0100

    Add europe.html

--- WeatherForecast/app.py
+++ WeatherForecast/app.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -35,6 +35,38 @@ def get_weather():
         return jsonify(data)
     else:
         return jsonify({'error': 'Nem sikerült lekérni az időjárási adatokat'}), 500
+    
+
+@app.route("/europe")
+def get_european_weather():
+    european_cities = ["Paris", "London", "Berlin", "Madrid", "Rome", "Athens", "Budapest", "Debrecen"]
+
+    european_weather = []
+
+    for city in european_cities:
+        params = {
+            'q': city,
+            'appid': API_KEY,
+            'units': 'metric'
+        }
+        response = requests.get(API_URL, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+            weather = {
+                'city': data['name'],
+                'temp': data['main']['temp'],
+                'feels_like': data['main']['feels_like'],
+                'humidity': data['main']['humidity'],
+                'temp_max': data['main']['temp_max'],
+                'temp_min': data['main']['temp_min'],
+                'description': data['weather'][0]['description']
+            }
+            
+            european_weather.append(weather)
+
+        
+    return render_template('europe.html', european_weather=european_weather)
 
 if __name__ == '__main__':
     app.run(debug=True)
